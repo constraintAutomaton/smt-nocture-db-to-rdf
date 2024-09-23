ccba3d5f2d627059ccb8ff2f3f4dabe2134774d2
Votre Nom <Vous@exemple.com>
2024-09-23 18:16:59 +0200

    made demon generation into a function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{builder::Str, Parser};
 use cli::CliArgs;
 use csv;
 use serde::Deserialize;
@@ -25,61 +25,90 @@ fn main() {
         raw_demon_file,
     } = CliArgs::parse();
 
-    let demon_rdf_file_namespace =
-        Namespace::new(demon_rdf_namespace.unwrap_or("http://example.org/".to_string())).unwrap();
-    let race_rdf_file_namespace =
-        Namespace::new(race_rdf_namespace.unwrap_or("http://example.org/".to_string())).unwrap();
+    let demon_rdf_namespace = demon_rdf_namespace.unwrap_or("http://example.org/".to_string());
+    let race_rdf_namespace = race_rdf_namespace.unwrap_or("http://example.org/".to_string());
+    let vocabulary_namespace = vocabulary_namespace.unwrap_or("http://example.org/".to_string());
+    let game_rdf_namespace = game_rdf_namespace.unwrap_or("http://example.org/".to_string());
 
-    let vocabulary_rdf_file_namespace =
-        Namespace::new(vocabulary_namespace.clone().unwrap_or("http://example.org/".to_string())).unwrap();
+    let out_path = out_path.unwrap_or(PathBuf::from("./output/"));
+    let path_vocabulary = path_vocabulary.unwrap_or(PathBuf::from("./vocabulary.ttl_template"));
+    let path_game = path_game.unwrap_or(PathBuf::from("./game.ttl_template"));
 
-    let mut race_transformer = RaceTransformer::new(&race_rdf_file_namespace, &vocabulary_rdf_file_namespace);
+    let raw_demon_file = raw_demon_file.unwrap_or(PathBuf::from("./demon_simple_info.csv"));
+
+    generate_demon_rdf(
+        demon_rdf_namespace,
+        race_rdf_namespace,
+        vocabulary_namespace,
+        game_rdf_namespace,
+        &out_path,
+        &path_vocabulary,
+        &path_game,
+        &raw_demon_file,
+    ).unwrap();
+    
+}
+
+fn generate_demon_rdf(
+    demon_rdf_namespace: String,
+    race_rdf_namespace: String,
+    vocabulary_namespace: String,
+    game_rdf_namespace: String,
+
+    out_path: &PathBuf,
+    path_vocabulary: &PathBuf,
+    path_game: &PathBuf,
+
+    raw_demon_file: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let demon_rdf_file_namespace = Namespace::new(demon_rdf_namespace)?;
+    let race_rdf_file_namespace = Namespace::new(race_rdf_namespace)?;
+
+    let vocabulary_rdf_file_namespace = Namespace::new(vocabulary_namespace.clone())?;
+
+    let mut race_transformer =
+        RaceTransformer::new(&race_rdf_file_namespace, &vocabulary_rdf_file_namespace);
     let mut demon_transformer = DemonTransformer::new(
         &demon_rdf_file_namespace,
         &race_rdf_file_namespace,
         &vocabulary_rdf_file_namespace,
     );
 
-    let out_folder = out_path.unwrap_or(PathBuf::from("./output/"));
+    let out_folder = out_path;
     let race_output_file = out_folder.join("race.ttl");
     let demon_output_file = out_folder.join("demon.ttl");
     let vocabulary_output_file = out_folder.join("vocabulary.ttl");
     let game_output_file = out_folder.join("game.ttl");
 
     rdf_from_template(
-        path_vocabulary.unwrap_or(PathBuf::from("./vocabulary.ttl_template")),
-        vocabulary_output_file,
-        vocabulary_namespace.unwrap_or("http://example.org/".to_string()),
-    )
-    .unwrap();
+        path_vocabulary,
+        &vocabulary_output_file,
+        vocabulary_namespace,
+    )?;
 
-    rdf_from_template(
-        path_game.unwrap_or(PathBuf::from("./game.ttl_template")),
-        game_output_file,
-        game_rdf_namespace.unwrap_or("http://example.org/".to_string()),
-    )
-    .unwrap();
+    rdf_from_template(path_game, &game_output_file, game_rdf_namespace)?;
 
-    let raw_file: File =
-        File::open(raw_demon_file.unwrap_or(PathBuf::from("./demon_simple_info.csv"))).unwrap();
+    let raw_file: File = File::open(raw_demon_file)?;
     let buf_reader = BufReader::new(raw_file);
 
     let mut rdr = csv::Reader::from_reader(buf_reader);
 
     for result in rdr.deserialize() {
-        let demon_record: Demon = result.unwrap();
+        let demon_record: Demon = result?;
 
         demon_transformer.demon.push(demon_record.clone());
         race_transformer.races.insert(demon_record.race);
     }
 
-    demon_transformer.to_file(demon_output_file).unwrap();
-    race_transformer.to_file(race_output_file).unwrap();
+    demon_transformer.to_file(demon_output_file)?;
+    race_transformer.to_file(race_output_file)?;
+
+    Ok(())
 }
 
 fn rdf_from_template(
-    templace_path: PathBuf,
-    out_file: PathBuf,
+    templace_path: &PathBuf,
+    out_file: &PathBuf,
     namespace: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let payload: String = fs::read_to_string(templace_path)?;

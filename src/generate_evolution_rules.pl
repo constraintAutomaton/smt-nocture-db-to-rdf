a6a798903ee7049f531d74f8b3060202c61fd334
constraintAutomaton <Vous@exemple.com>
2025-05-10 23:37:34 +0200

    evolution generator made

--- src/generate_evolution_rules.pl
+++ src/generate_evolution_rules.pl
@@ -6,49 +6,53 @@
 :- use_module('./util.pl').
 
 generate_evolution_rules(Iri, VocabPrefix, DemonPrefix) :- 
-    phrase_from_file(json_chars(Json), '../demon_data/evolutions.json')
-    , open('../output/evolution_rules.ttl', write, Stream)
-    , evolution_info(Json, VocabPrefix, DemonPrefix, Triples)
-    , license(Iri, License)
-    , maplist(write(Stream), License)
-    , write(Stream, '\n\n')
-    , append(Triples, TriplesFlatten)
-    , maplist(write(Stream), TriplesFlatten)
-    , close(Stream)
-    .
+    phrase_from_file(json_chars(Json), '../demon_data/evolutions.json'),
+    open('../output/evolution_rules.ttl', write, Stream),
+    evolution_info(Json, VocabPrefix, DemonPrefix, Triples),
+    license(Iri, License),
+    maplist(write(Stream), License),
+    write(Stream, '\n\n'),
+    append(Triples, TriplesFlatten),
+    maplist(write(Stream), TriplesFlatten),
+    close(Stream).
 
 evolution_info(pairs([string(Name)-pairs([string("lvl")-number(Lv), string("result")-string(Result)])|Rest]), VocabPrefix, DemonPrefix, Triples) :-
-    evolution_info_([string(Name)-pairs([string("lvl")-number(Lv), string("result")-string(Result)])|Rest], VocabPrefix, DemonPrefix, Triples)
-    .
+    evolution_info_([string(Name)-pairs([string("lvl")-number(Lv), string("result")-string(Result)])|Rest], VocabPrefix, DemonPrefix, Triples).
 
 evolution_info_([], _, _, []).
 
 evolution_info_([string(Name)-pairs([string("lvl")-number(Lv), string("result")-string(Result)])|Rest], VocabPrefix, DemonPrefix, Triples):-
-    evolution_info_(Rest, VocabPrefix, DemonPrefix, Triple2)
-    , iri_rule_declaration(Name, RuleIri)
-    , rule_declaration_triple(RuleIri, VocabPrefix, TripleDeclaration)
-    , level_evolution_triple(Lv, VocabPrefix, LvTriple)
-    , result_triple(Result, DemonPrefix, VocabPrefix, ResultTriple)
-    , append([TripleDeclaration], [LvTriple], [ResultTriple], TripleSet)
-    , append([Triple2, TripleSet], Triples)
-    .
+    evolution_info_(Rest, VocabPrefix, DemonPrefix, Triple2),
+    iri_rule_declaration(Name, RuleIri),
+    rule_declaration_triple(RuleIri, VocabPrefix, TripleDeclaration),
+    level_evolution_triple(Lv, VocabPrefix, LvTriple),
+    result_triple(Result, DemonPrefix, VocabPrefix, ResultTriple),
+    append([[TripleDeclaration], [LvTriple], [ResultTriple]], TripleSet),
+    append([Triple2, TripleSet], Triples).
 
 iri_rule_declaration(Name, RuleIri):-
-    append(["<",Name, ">"], RuleIri)
-    .
+    (
+        replace_space(Name, NameCurated, "_") ->
+        true
+    ;   NameCurated=Name
+    ),
+    append(["<", NameCurated, ">"], RuleIri).
 
 rule_declaration_triple(RuleIri, VocabPrefix, Triple) :-
-    append([RuleIri, " a ", VocabPrefix, "EvolutionRule", ";\n"], Triple)
-    .
+    append([RuleIri, " a ", "<", VocabPrefix, "EvolutionRule", ">;\n"], Triple).
 
 level_evolution_triple(Lv, VocabPrefix,  Triple):-
-    append(["\t", VocabPrefix, "evolutionLevel ", Lv, ";\n" ], Triple)
-    .
+    number_chars(Lv, LvChar),
+    append(["\t","<", VocabPrefix, "evolutionLevel> ", LvChar, ";\n" ], Triple).
 
 result_triple(Result, DemonPrefix, VocabPrefix, Triple) :-
-    append(["<", DemonPrefix, Result, ">"], DemonTerm)
-    , append(["\t", VocabPrefix, "demonResult ", DemonTerm, ".\n" ], Triple)
-    .
+    (
+        replace_space(Result, ResultCurated, "_") ->
+        true
+    ;   ResultCurated=Result
+    ),
+    append(["<", DemonPrefix, ResultCurated, ">"], DemonTerm),
+    append(["\t", "<", VocabPrefix, "demonResult> ", DemonTerm, ".\n" ], Triple).
 
 license(Iri, License) :-
 Template = "# This data  is made available under the Open Database License: http://opendatacommons.org/licenses/odbl/1.0/.\n\
--- src/util.pl
+++ src/util.pl
@@ -4,4 +4,8 @@ replace_template(Xs0, Xs1, Element):-
     append([Front, "{}" ,Rest], Xs0),
     append([Front, Element ,Rest], Xs1).
 
+replace_space(Xs0, Xs1, Element):- 
+    append([Front, " " ,Rest], Xs0),
+    append([Front, Element ,Rest], Xs1).
+
 fusion_chart(Json) :- phrase_from_file(json_chars(Json) , '../demon_data/fusion-chart.json').
